eb048ae6f4922507c1aec973e8bafcd462d73426
qowns8 <qowns8@madup.com>
2018-05-18 07:49:03 +0900

    네이버 모바일 관광지 크롤링

--- src/main/scala/Crawler.scala
+++ src/main/scala/Crawler.scala
@@ -10,32 +10,57 @@ import scala.io.Source
 
 object Crawler {
 
-  System.setProperty("webdriver.chrome.driver", "chromedriver_mac")
+  // 리눅스 크롬 - 셀레늄
+  System.setProperty("webdriver.chrome.driver", "chromedriver_linux")
   implicit val webDriver: WebDriver = new ChromeDriver()
+  // 스레드 timeout 설정
   val waitDriver = new WebDriverWait(webDriver, 30)
-
   webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS)
 
+  // 관광지 위치들
   def readLocals = Source.fromFile("src/resources/local.txt").getLines().toList
 
+  /**
+    * 관광지 이름을 url로 인코딩한 리스트
+    * @return List[String]
+    * @example {{{
+    *           val urls = Crawler.getUriList
+    *           urls.map(eachUrl => {
+    *             // something work
+    *           })
+    * }}}
+    */
   def getUriList = readLocals.map(line => s"https://m.search.naver.com/search.naver?query=${URLEncoder.encode(line , "UTF-8")}&sm=mtb_hty.top&where=m")
 
+  /**
+    * html에서 이미지, 랭킹, 태그, 이름을 Map[String, String]으로 가저오는 메소드
+    * @param index 번호
+    * @return Map[String, String]
+    * @example {{{
+    *     var count = 0
+    *     urls.map(eachUrl => {
+    *       // something do
+    *       parsing(count)
+    *     })
+    * }}}
+    */
   def parsing(index: Long) = {
+    // 랭크, 태그, 이름
     val selector = s"#_sau_overseas_trip_answer > div.api_subject_bx > div.cs_attraction._lp_animation._attraction_list_root > div.attraction_list > div > div > div > ul > li:nth-child(${index})"
     val eachItem = By.cssSelector(selector)
     waitDriver.until(ExpectedConditions.presenceOfElementLocated(eachItem))
-    val items = webDriver.findElement(eachItem).getText
-    val itemList = items.split("\n")
+    val items = webDriver.findElement(eachItem).getText.split("\n")
 
+    // 이미지
     val imgSelector = s"${selector} > a > span > img"
     val eachImg = By.cssSelector(imgSelector)
     waitDriver.until(ExpectedConditions.presenceOfElementLocated(eachImg))
     val img = webDriver.findElement(eachImg).getAttribute("src")
 
     Map (
-      "rank" -> itemList(0),
-      "name" -> itemList(1),
-      "tag" -> itemList(2),
+      "rank" -> items(0),
+      "name" -> items(1),
+      "tag" -> items(2),
       "img" -> img
     )
   }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,21 +8,21 @@ object Main {
   protected implicit lazy val jsonFormats: Formats = DefaultFormats
 
   def main(args: Array[String]): Unit = {
-    val range = Range(1, 7).toList
-
-    val locals = Crawler.readLocals
-    var count = -1
+    val range = Range(1, 7).toList // 각 관광지당 상위 7개만 뽑음
+    val locals = Crawler.readLocals // 관광지 이름들
+    var count = -1 // 관광지 카운터
     val results = Crawler.getUriList.map(url => {
       count = count + 1
-      Crawler.webDriver.navigate.to(url)
-      range.map(index => {
-        Crawler.parsing(index) ++ Map("local" -> locals(count))
+      Crawler.webDriver.navigate.to(url) // 페이지 이동
+      range.map(index => { // 1에서 7까지
+        Crawler.parsing(index) ++ Map("local" -> locals(count)) // 파싱데이터 + 지역이름
       })
     })
-    println(results)
+    println(results) // log
 
     val jsonStr = Serialization.write(results)
 
+    //파일 입출력
     val file = new File("landmark_result.txt")
     val bw = new BufferedWriter(new FileWriter(file))
     bw.write(jsonStr)
